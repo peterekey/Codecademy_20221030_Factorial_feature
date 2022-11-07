824bdf2dc26cf84bd73785757ce890e195420a26
Pete <63628694+peterekey@users.noreply.github.com>
2022-11-07 11:09:51 +1100

    Add solutions

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const Calculate = {
-  
+  factorial(num) {
+    let result
+    if (num != 0) {
+        result = num;
+        for(let i = num - 1; i > 0; i--) {
+            result *= i;
+         }
+
+    } else {
+        result = 1;
+    } 
+    return result;
+  }
 }
 
 module.exports = Calculate;
--- index.test.js
+++ index.test.js
@@ -1,8 +1,44 @@
 var assert = require("assert");
-var Calculate =  require('../index.js')
+var Calculate =  require('./index.js')
 
 describe('Calculate', () => {
   describe('.factorial', () => {
+    it('returns 120 for a 5!', () => {
+      // Setup
+      const number = 5
+      const expectedOutput = 120
 
+      // Exercise
+      const result = Calculate.factorial(number);
+
+      // Verify
+      assert.equal(result, expectedOutput);
+
+      // Step 2: assert.equal(Calculate.factorial(5), 120);
+    })
+
+    it('returns 6 for a 3!', () => {
+      // Setup
+      const input = 3;
+      const expectedOutput = 6;
+
+      // Exercise
+      const result = Calculate.factorial(input);
+
+      // Verify
+      const output = assert.equal(result, expectedOutput);
+    });
+
+    it('returns 1 for 0!', () => {
+      // Setup
+      const input = 0;
+      const expectedOutput = 1;
+
+      // Exercise
+      const result = Calculate.factorial(input);
+
+      // Verify 
+      const output = assert.equal(result, expectedOutput);
+    });
   });
 });
